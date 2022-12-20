e2dde80d6394a844e378907ba24aae93477cbcd8
sag1011 <sag1011@alu.ubu.es>
2022-12-20 18:04:12 +0100

    Update log-in.py

--- py/log-in.py
+++ py/log-in.py
@@ -1,1 +1,8 @@
-Tarea de log-in en desarrollo
+DESARROLLO:
+    -Esta funcion es para poder iniciar sesion en la aplicacion desde un dispositivo.
+
+CONDICIONES DE SATISFACCIÓN:
+    -El usuario y la contraseña tienen que estar correctos.
+
+DEPENDENCIAS:
+    -La cuenta debe existir.
